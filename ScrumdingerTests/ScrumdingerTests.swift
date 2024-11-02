59847d19227fe9a54757414b83273ab8fe9b30e6
Tanveer Ashraf <it.3xpert@gmail.com>
2024-11-02 13:30:22 +0000

    #1 Added card view for scrums

---
+++ ScrumdingerTests/ScrumdingerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerTests.swift
+//  ScrumdingerTests
+//
+//  Created by Tanveer Ashraf on 02/11/2024.
+//
+
+import Testing
+@testable import Scrumdinger
+
+struct ScrumdingerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
